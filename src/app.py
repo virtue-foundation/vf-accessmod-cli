200d0a85f98729e1411a862958f5f4a9d1cd242e
Corey <17171838+coreyryanhanson@users.noreply.github.com>
2022-12-09 13:46:23 -0500

    implementing manual file transfer for when shared volumes are not an option

--- src/app.py
+++ src/app.py
@@ -4,12 +4,15 @@ import os
 import subprocess
 from threading import Thread
 
-from flask import Flask, request
+from flask import Flask, flash, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
 @dataclass
 class JobRunner:
     check_endpoint = "/check"
+    file_transfer_endpoint = "/file_transfer"
     landcover_endpoint = "/merge_landcover"
     error = False
     last_endpoint = None
@@ -37,6 +40,12 @@ class JobRunner:
 
 
 job_runner = JobRunner()
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           os.path.splitext(filename)[1].lower() in [".csv", ".tif", ".img", ".geojson"]
+
 
 def single_job_only(func):
     """Raises an error if a job is already running."""
@@ -97,6 +106,36 @@ def landcover_request():
     Thread(target=run_merge_landcover, args=args).start()
     return job_runner.status_json(), 202
 
+
 @app.get(job_runner.check_endpoint)
 def check_request():
     return job_runner.status_json(), 200
+
+
+@app.route(job_runner.file_transfer_endpoint, methods=['GET', 'POST'])
+def file_transfer():
+    def _download():
+        filename = request.get_json()["filename"]
+        return send_from_directory(directory=paths.path_output, filename=filename)
+
+    def _upload():
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config[paths.path_output], filename))
+            return redirect(url_for('download_file', name=filename))
+
+    region = request.get_json()["region"]
+    paths = FilePathHandler(region)
+    if request.method == 'POST':
+        return _upload()
+    return _download()
