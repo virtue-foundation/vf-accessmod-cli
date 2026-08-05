30c70511f2433468f42b5811cc2d4e2e0dac1b05
Corey Ryan Hanson <17171838+coreyryanhanson@users.noreply.github.com>
2026-08-05 10:04:27 +0000

    chore: remove dead config constants and commented-out lines in config.R

--- src/config.R
+++ src/config.R
@@ -4,18 +4,12 @@ require(jsonlite)
 require(DBI)
 
 require(optparse)
-# require(geojsonio)
 
 config <- list()
-
-# config$sepClass <- ""
-
 
 config$gisBase <- Sys.getenv(x = "GISBASE")
 # setwd(dirname(dirname(parent.frame(2)$ofile)))
 config$GrassDataBase <- Sys.getenv(x = "GISDBASE")
-# config$pathCacheDir <- normalizePath("/data/cache/")
-# config$pathGrassDemo <- normalizePath("config/data/demo")
 config$pathDictDir <- normalizePath("dictionary/")
 config$pathDictMain <- normalizePath(file.path(config$pathDictDir, "main.json"))
 config$pathClasses <- file.path(config$pathDictDir, "classes.json")
@@ -33,10 +27,6 @@ config$listTranspMod <- list(
 )
 
 config$mapDem <- "r_dem"
-
-# character separator
-config$sepTagFile <- "_"
-config$sepClass <- "__"
 
 source("functions.R")
 print("Loaded all functions")
